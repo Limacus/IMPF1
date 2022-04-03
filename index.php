97643bb2e7b7dc5518f7485f09f77e26ad68f435
Limacus <daviesliam021@gmail.com>
2022-04-03 21:12:18 +0100

    add php stuff

--- index.php
+++ index.php
@@ -4,15 +4,8 @@
       <link rel="stylesheet" href="style.css">  
       <script src="scripts.js"></script>
     <body>
-      <header>
-      </header>
-      <div class="wrapper">
-      <p class="test-1">What a cahd lobstahrin’ dinnahbucket windah bendah dinnahbucket slower than molasses going uphill in January some cunnin over t’, numb Bah Hahbah Outta Staydahs tunk scrod native, nummah flatlandas railed ’em hahd tellin’ not knowin’ unthaw, ‘Roostik Ahcadiuh over t’ cah yow uns Bean’s fish chowdah anuthah, Sundee ayuh wicked pissah, wee bit nippy Katahdin Jo-Jeezly dingy stove up.
-
-      </p>
-      <p class="test-2">Hum-dingah well theyah suppah cunnin’ batrees grindah bub puff, Bangah clam chowdah numb, Shit the bed. Feed ‘uh the hot suppah. stove-up justa smidgin podunk stove-up tube steak up t’, Bangoah no-see-um Katahdin. Wee bit nippy Loyston-Ahban geez bud chimbly p’dayduhs stove-up chimbly, windah bendah Saddee yut p’dayduhs If you can’t stand the wintah you don’t deserve the summah sawr got in a gaum The County, yessah-bub bub Shit the bed. Feed ‘uh the hot suppah. Mahdin’s, wee bit nippy moose noseeum, what a cahd lobstahrin’ Katahdin Jeesum Crow muckle riyht on’ta her.
-
-      </p>
-    </div>
+      <?php include('header.php'); ?>
+      <?php include('content-home.php'); ?>
+    <?php include('footer.php'); ?>
   </body>
 </html>
